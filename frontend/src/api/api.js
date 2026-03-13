03d93037ed81b1d9d79793dae566678c06ca2b03
customsoftteam <125879469+customsoftteam@users.noreply.github.com>
2026-03-13 17:19:29 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: "https://machine-failure-prediction-7mnf.onrender.com",
   timeout: 60000,
 });
 
